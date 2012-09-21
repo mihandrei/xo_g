d94cbdb74540dc2cea8b4b027784336a1d044c8f
Mihai Andrei <mihai.daniel.psmg@gmail.com>
2012-09-21 22:52:01 +0300

    an elegant game model;

--- xo.fsx
+++ xo.fsx
@@ -1,60 +1,37 @@
-type coord = {r:int; c:int}
-type token = X|O
-type CellContent = token option 
+type Coord = int*int
+type Token = X|O
+type CellContent = Token option 
 
-//type systemu e aproape complet in cazu asta. board mai poate fi invalid daca are lungimea != 9
-//desi o reprezentare ca stringuri "A1" e mai flexibila si impreuna cu validare la constructie mai robusta
-type Board = Map<coord , CellContent>    
+type Board = Map<Coord , CellContent>    
 
-let emptyBoard : Board = 
-    [for j in 1..3 do
-        for i in 1..3 do
-            yield {r=i; c=j}, None]
-    |> Map.ofList
+//precompute all rays
+//as they are a few for this game
+//matlab style: rows = meshgrid(1..3 1..3)  columns = rows'; diags = toepl [1]; toepl[1]'
+let rows =    [for c in 1..3 -> [for r in 1..3 -> (r,c)]]              
+let columns = [for r in 1..3 -> [for c in 1..3 -> (r,c)]]                  
+let diags =   [[for r in 1..3 -> (r,r)]; [for r in 1..3 -> (r,4-r)]]
+//I suppose this does not allocate a new list
+let rays = Seq.concat [rows; columns ; diags]
 
-let move (board:Board) c cell = 
+//let zipWithConst s c = Seq.zip s (Seq.initInfinite (fun _ -> c))
+
+let board_of_list l : Board = 
+    let coords = List.concat rows    
+    List.zip coords l |> Map.ofList
+
+let emptyBoard  = 
+    board_of_list (List.replicate (3*3) None)
+
+let move c cell (board:Board)= 
     board.Add (c , cell)
 
-let newb = move emptyBoard {r=1; c=1} (Some X)
+let str_board (board:Board) =     
+    let cellstr = function None -> "_" | Some O -> "0" | Some X -> "X"
 
-let zm = Map.ofList [(1, 'a'); (2, 'b'); (3, 'c')]
-zm.Add (1,'b')
+    [for r in rows do
+        yield [for c in r -> cellstr board.[c]] |> String.concat ""
+    ] |> String.concat "\n" 
 
-type board = Map<int , token option>
-
-let display_board (board:Board) =     
-    let cellstr = function X->'X' | O->'0' | E->'_'
-    board 
-    |> Map.fold (fun s _ v -> s + (cellstr v)) 
-
-    sprintf "%A%A%A\n%A%A%A\n%A%A%A" 2 3    
-    let pa = "%A"
-    sprintf pa 43
-    ()
-        
-let lines = [//linii
-                [ R1,C1 ; R1,C2 ; R1,C3]
-                [ R2,C1 ; R2,C2 ; R2,C3]
-                [ R3,C1 ; R3,C2 ; R3,C3]
-                //coloane
-                [ R1,C1 ; R2,C1 ; R3,C1]
-                [ R2,C2 ; R2,C2 ; R3,C2]
-                [ R2,C3 ; R2,C3 ; R3,C3]
-                //diag
-                [ R1,C1 ; R2,C2 ; R3,C3]
-                [ R1,C3 ; R2,C2 ; R3,C1]
-            ]
-
-let goal board = 
-    let eval_line line = 
-        let z = line |> List.map |> Map.find
-        z
-
-type state  = 
-    {
-     children : node<'a> list
-     value : 'a
-    }
-
-
-let kd = '_'*4
+emptyBoard
+|> move (1,1) (Some X)
+|> str_board 

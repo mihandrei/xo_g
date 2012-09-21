0b3f0f590cc2d484de33771c3f8d120469b0f8d4
Mihai Andrei <mihai.daniel.psmg@gmail.com>
2012-09-22 02:37:15 +0300

    expand and fixes
    implemented expand
    fixed: rows and colums were swapped
    improved board formatting

--- xo.fsx
+++ xo.fsx
@@ -6,9 +6,9 @@ type Board = Map<Coord , Cell>
 //precompute all rays
 //as they are a few for this game
 //matlab style: rows = meshgrid(1..3 1..3)  columns = rows'; diags = toepl [1]; toepl[1]'
-let rows =    [for c in 1..3 -> [for r in 1..3 -> (r,c)]]              
-let columns = [for r in 1..3 -> [for c in 1..3 -> (r,c)]]                  
-let diags =   [[for r in 1..3 -> (r,r)]; [for r in 1..3 -> (r,4-r)]]
+let rows    =  [for r in 1..3 -> [for c in 1..3 -> (r,c)]]                  
+let columns =  [for c in 1..3 -> [for r in 1..3 -> (r,c)]]              
+let diags   = [[for r in 1..3 -> (r,r)]; [for r in 1..3 -> (r,4-r)]]
 //I suppose this does not allocate a new list
 let rays = Seq.concat [rows; columns ; diags]
 
@@ -25,12 +25,10 @@ let move c cell (board:Board) = board.Add (c , cell)
 let str_board (board:Board) =     
     let cellstr = function E -> "_" | O -> "0" | X -> "X"
     [for r in rows do
-        yield [for c in r -> cellstr board.[c]] |> String.concat ""
+        yield [for c in r -> cellstr board.[c]] |> String.concat " "
     ] |> String.concat "\n" 
-    
-emptyBoard
-|> move (1,1) X
-|> str_board 
+
+let print_board = str_board >> printfn "%s"     
 
 let goal (board:Board) =
     let value_of_ray ray = [for c in ray -> board.[c]]
@@ -43,14 +41,29 @@ let goal (board:Board) =
     |> Option.map (fun ray -> board.[ray.Head], ray)
     
 
-let expand (board:Board) = 
-    ()
+let expand cell (board:Board) : seq<Board> = 
+    //rahat cand iterezi un Map nu primesti tuple ci KeyValuePair
+    seq {for kw in board do
+            if kw.Value=E then 
+                yield move kw.Key cell board
+        }
 
 let minmax (board:Board) side = 
     ()
 
-[X;E;X 
- E;X;E
- X;E;E;] 
-|> board_of_list 
-|> goal
+//facut teste din astea
+emptyBoard
+|> move (1,1) X
+|> print_board 
+
+let b=
+    [X;E;X 
+     E;X;E
+     X;X;E;] 
+    |> board_of_list 
+
+b|> print_board
+
+b|> goal
+
+b|> expand O |> Seq.iter (fun x -> print_board x ; printfn "\n")
